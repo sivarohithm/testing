b5b4f9d46786193f1e4e1ff561afed5c3359228d
root <root@ip-172-31-45-242.us-west-2.compute.internal>
2016-07-25 08:02:24 +0000

    unit test class

---
+++ TestBankAccount.java
@@ -0,0 +1,17 @@
+package store;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBankAccount {
+
+@Test
+
+public void testDebit(){
+
+BankAccount account = new BankAccount(10);
+double amount = account.debit(5);
+Assert.assertEquals(5.0, amount);
+}
+
